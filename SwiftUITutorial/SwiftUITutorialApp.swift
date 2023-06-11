1e9974f020c5bb8e22e34fb5c10afe00aa87d520
0ofKim <pos00042@naver.com>
2023-06-11 23:07:42 +0900

    First Commit (Creating and Combining Views)

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by 김의영 on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
